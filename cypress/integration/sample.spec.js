45fea936b7bddd88b005de400a1aa03bad3fb459
Tuyen Hoang <tuyen.hoangvan@powergatesoftware.com>
2022-07-18 15:54:01 +0700

    Add github action

--- cypress/integration/sample.spec.js
+++ cypress/integration/sample.spec.js
@@ -4,6 +4,6 @@ context('Check cypress run successfully!', function() {
   })
 
   it('To verify open correct site', function() {
-    cy.get('h3').contains('Fun Chat 100').should('exist')
+    cy.get('h3').contains('Fun Chat - test').should('exist')
   });
 })
