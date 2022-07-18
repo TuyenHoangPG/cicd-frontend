cf9f0711e4a3154238d064513af861a2c80478cb
Tuyen Hoang <tuyen.hoangvan@powergatesoftware.com>
2022-07-18 16:10:29 +0700

    Change config

--- cypress/integration/sample.spec.js
+++ cypress/integration/sample.spec.js
@@ -3,7 +3,11 @@ context('Check cypress run successfully!', function() {
     cy.visit(Cypress.config().baseUrl);
   })
 
-  it('To verify open correct site', function() {
+  it('To verify open correct site fail case', function() {
     cy.get('h3').contains('Fun Chat - test').should('exist')
+  });
+
+  it('To verify open correct site pass case', function() {
+    cy.get('h3').contains('Fun Chat').should('exist')
   });
 })
